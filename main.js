158fc74964719d0be70806136a26c9bd9b3b6f1c
Areeb <areebahmed8666@gmail.com>
2024-12-16 14:20:59 +0500

    Assignment 12

---
+++ main.js
@@ -0,0 +1,15 @@
+let todos = [];
+
+let pr1 = prompt('Enter Your First todo')
+todos.push(pr1)
+let pr2 = prompt('Enter Your second todo')
+todos.push(pr2)
+let pr3 = prompt('Enter Your third todo')
+todos.push(pr3)
+let pr4 = prompt('Enter Your fourth todo')
+todos.push(pr4)
+
+console.log("Your-todo-list")
+console.log(todos)
+
+alert("Your todo list see on console")
